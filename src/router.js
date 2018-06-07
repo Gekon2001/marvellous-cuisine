046aa6f30bd8f0113b200a65ce2949d2373fafb1
Dmitriy Vasin <vasindmitriy1992@gmail.com>
2018-06-07 20:40:48 +0300

    add gallery

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import App from 'Components/app/App';
 import LoginSignup from 'Components/login/LoginSignup';
 import Home from 'Components/home/Home';
 import NotFound from 'Components/notfound/NotFound';
+import Gallery from "./components/gallery/Gallery";
 
 const history = createBrowserHistory();
 
@@ -16,6 +17,7 @@ export default function Router() {
       <App>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/gallery' component={Gallery} />
           <Route path='/login' component={LoginSignup} />
           <Route path={'*'} component={NotFound} />
         </Switch>
